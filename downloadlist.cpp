145b1b8e30441344b5a46198bb23a5d76fb815ed
Maicss <maicss@126.com>
2020-07-28 10:38:03 +0800

    修复 下载未完成时断网，会显示下载成功

--- downloadlist.cpp
+++ downloadlist.cpp
@@ -13,10 +13,10 @@ downloadlist::downloadlist(QWidget *parent) :
     ui(new Ui::downloadlist)
 {
     ui->setupUi(this);
-    ui->pushButton->setEnabled(false);
+    ui->pushButton_install->setEnabled(false);
     ui->progressBar->setValue(0);
     ui->label_filename->hide();
-    ui->pushButton->hide();
+    ui->pushButton_install->hide();
     ui->pushButton_3->hide();
     ui->widget_spinner->start();
     ui->widget_spinner->hide();
@@ -53,13 +53,24 @@ QString downloadlist::getName()
 
 void downloadlist::readyInstall()
 {
+    if(ui->progressBar->value()!= ui->progressBar->maximum()){
+        ui->progressBar->hide();
+        ui->pushButton_install->show();
+        ui->pushButton_2->hide();
+        system("notify-send \""+ui->label->text().toUtf8()+"下载失败\"" +" --icon=/tmp/spark-store/icon_"+QString::number(num).toUtf8()+".png");
+        ui->label_2->setText("下载失败，可能是网络错误");
+        ui->pushButton_install->setEnabled(false);
+        return;
+
+    }
     if(!close){
         ui->progressBar->hide();
-        ui->pushButton->setEnabled(true);
-        ui->pushButton->show();
+        ui->pushButton_install->setEnabled(true);
+        ui->pushButton_install->show();
         ui->pushButton_2->hide();
         system("notify-send \""+ui->label->text().toUtf8()+"下载完成，等待安装\"" +" --icon=/tmp/spark-store/icon_"+QString::number(num).toUtf8()+".png");
     }
+
 }
 
 void downloadlist::setFileName(QString fileName)
@@ -86,7 +97,7 @@ void downloadlist::on_pushButton_clicked()
 {
     if(!isInstall){
         isInstall=true;
-        ui->pushButton->hide();
+        ui->pushButton_install->hide();
         ui->widget_spinner->show();
         qDebug()<<"/tmp/spark-store/"+ui->label_filename->text().toUtf8();
         ui->label_2->setText("正在安装，请稍候");
@@ -117,20 +128,20 @@ void downloadlist::on_pushButton_clicked()
             isInstall.waitForFinished();
             int error=QString::fromStdString(isInstall.readAllStandardError().toStdString()).length();
             if(error==0){
-                ui->pushButton->hide();
+                ui->pushButton_install->hide();
                 ui->label_2->setText("安装完成");
                 ui->pushButton_3->show();
             }else {
-                ui->pushButton->show();
-                ui->pushButton->show();
-                ui->pushButton->setText("重装");
+                ui->pushButton_install->show();
+                ui->pushButton_install->show();
+                ui->pushButton_install->setText("重装");
                 ui->label_2->setText("安装出现错误，可重新安装");
                 ui->pushButton_3->show();
             }
             if(notRoot){
                 ui->label_2->setText("安装被终止，可重新安装");
-                ui->pushButton->show();
-                ui->pushButton->show();
+                ui->pushButton_install->show();
+                ui->pushButton_install->show();
                 ui->pushButton_3->hide();
             }
             ui->widget_spinner->hide();
